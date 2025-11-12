ba2d4b2c2d9381e6946c5beeae44ba9ebdca7e75
GoldenTW <kjhuangf@icloud.com>
2025-11-12 09:22:35 +0000

    1722

--- Lesson8/Lesson8_2.py
+++ Lesson8/Lesson8_2.py
@@ -1,12 +1,31 @@
 import os
+import random
 
-relative_path = 'Lesson8/assets/names.txt'
-absolute_path = os.path.abspath(relative_path)
+def get_names(file_name):
+
+    relative_path = "assets/" + file_name
+    absolute_path = os.path.abspath(relative_path)
+
+    with open(absolute_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    return content.split('\n')
+
+def generate_student_scores(names, num=10):
+    Names_randomN = random.sample(names, num)
+    scores_list = []
+    for name in Names_randomN:
+        info = {"姓名": name,
+                "國文": random.randint(60, 100),
+                "數學": random.randint(60, 100),
+                "英文": random.randint(60, 100)
+            }
+        scores_list.append(info)                        
+    return scores_list
 
 
-with open(absolute_path, 'r', encoding='utf-8') as f:
-    content = f.read()
+names = get_names("names.txt")
+num = int(input("請輸入要產生幾位學生的成績資料: "))
+students = generate_student_scores(names, num=num)
 
-names_list = content.split(',')
-
-print(names_list)
+print(students)
--- Lesson8/Lesson8_2_1.py
+++ Lesson8/Lesson8_2_1.py
@@ -5,6 +5,6 @@ file_name ="/workspaces/Test_AI/Lesson8/assets/names.txt"
 with open(file_name, 'r', encoding='utf-8') as f:
     content = f.read()
 
-names_list = content.split(',')
+names_list = content.split('\n')
 
 print(names_list)
