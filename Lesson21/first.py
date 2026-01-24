b1c96103e57465ad1527654368f987f265f9fbc4
Golden <kjhuangf@icloud.com>
2026-01-24 21:22:50 +0800

    2122

--- Lesson21/first.py
+++ Lesson21/first.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return '<h1> Hello, Flask! </h1>'
+    return '<h1> Hello, Flask! change</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True)
